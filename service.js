33fd77f8f582264e50e8df6363c8c19fe1fa15ed
Jotasenpai <95491542+Jotasenpai@users.noreply.github.com>
2025-04-14 10:36:06 +0200

    Update service.js

--- service.js
+++ service.js
@@ -20,32 +20,24 @@ app.get('/render', async (req, res) => {
       });
       
     const page = await browser.newPage();
-    page.setDefaultNavigationTimeout(60000);
+    //page.setDefaultNavigationTimeout(60000);
 
     // Navega a la URL proporcionada
-    console.log(`Cargando la página: ${url}`);
     await page.goto(url, { timeout: 0, waitUntil: 'networkidle2' }); // Espera a que la página cargue completamente
-    
-    console.log(`Goto pagina`);
 
     // Espera un tiempo para que el JavaScript (como el model-viewer) se ejecute
     await new Promise(resolve => setTimeout(resolve, 10000)); // Ajusta el tiempo según lo que tarde en cargar el model-viewer
-    
-    console.log(`Modelo cargado`);
 
     // Opcional: Captura una captura de pantalla de la página
     const screenshot = await page.screenshot({ fullPage: true });
-    console.log(`Imagen capturada`);
 
     // Cierra el navegador
     await browser.close();
-    console.log(`He cerrado el navegador`);
 
     // Devuelve la captura de pantalla como respuesta
     res.set('Content-Type', 'image/jpeg');
     res.send(screenshot);
     
-    console.log(`Imagen generada`+screenshot);
   } catch (error) {
     console.error('Error al procesar la página:', error);
     res.status(500).send('Ocurrió un error al procesar la página.');
@@ -54,5 +46,4 @@ app.get('/render', async (req, res) => {
 
 // Inicia el servidor
 app.listen(port, () => {
-  console.log(`Servicio corriendo en http://localhost:${port}`);
 });
